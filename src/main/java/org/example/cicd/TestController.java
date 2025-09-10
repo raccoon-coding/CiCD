6cb16e877c47a8be17359dbdd41194b45f10e6d8
raccoon-coding <minsungt@naver.com>
2025-09-10 10:10:36 +0900

    first commit

---
+++ src/main/java/org/example/cicd/TestController.java
@@ -0,0 +1,16 @@
+package org.example.cicd;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teset")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity get() {
+        return ResponseEntity.ok("확인 완료");
+    }
+}
