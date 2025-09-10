b2be94454f9936ac67d46dd4d97fea8f6dc28e70
raccoon-coding <minsungt@naver.com>
2025-09-10 16:31:11 +0900

    test

--- src/main/java/org/example/cicd/TestController.java
+++ src/main/java/org/example/cicd/TestController.java
@@ -12,7 +12,7 @@ public class TestController {
     @GetMapping
     public ResponseEntity get() {
         System.out.println("test2");
-        return ResponseEntity.ok("확인 완료");
+        return ResponseEntity.ok("무중단 배포 확인 완료");
     }
 
     @GetMapping("/test")
