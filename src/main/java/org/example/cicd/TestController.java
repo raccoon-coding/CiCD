c3f8504548132fcf3637836c41ae98fdb3449716
raccoon-coding <minsungt@naver.com>
2025-09-10 15:20:09 +0900

    test

--- src/main/java/org/example/cicd/TestController.java
+++ src/main/java/org/example/cicd/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/teset")
+@RequestMapping("/test")
 public class TestController {
 
     @GetMapping
